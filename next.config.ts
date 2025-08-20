ec201d08cba59350ef92d062a592098066b32703
unknown <ravikant.medcapsky@gmail.com>
2025-08-20 17:54:42 +0530

    buil2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // enables static export
-  basePath: "/GoMedGo-website", // repo name for GitHub Pages
+  // basePath: "/", // repo name for GitHub Pages
   images: { unoptimized: true }, // fixes Next Image
 };
 
