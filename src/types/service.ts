dba7a6aa289d977180a1495fd374ed55349080de
unknown <ravikant.medcapsky@gmail.com>
2025-08-26 01:17:06 +0530

    update service type

--- src/types/service.ts
+++ src/types/service.ts
@@ -1,9 +1,15 @@
-// Define the shape of a service
 export interface Service {
   id: number;
   title: string;
   description: string;
   details: string;
+  details1?: string;
+  details2?: string;
+  headingOne: string;
   features: string[];
+  headingTwo: string;
+  features2: string[];
+  paragraph: string;
+  LastLine?: string;
   image: string;
 }
